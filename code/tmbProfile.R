e07ee56a01df889d680ec50f8bf2b49db5c2ad05
komalsrathi <komal.rathi@gmail.com>
2020-01-21 22:51:20 -0500

    new TMB plot

--- code/tmbProfile.R
+++ code/tmbProfile.R
@@ -1,64 +1,32 @@
-tmbProfile <- function(myTMB=tmbData) {
+############################
+# TMB Profile
+############################
+
+tmbProfile <- function(pedTMBScores = pedTMB, adultTMBScores = adultTMB, TMB) {
   
-  #Determine if sample is an outlier in dataset
-  isOutlier <- function(distTmp) {
-    c25 <- as.numeric(quantile(distTmp, .25))
-    c75 <- as.numeric(quantile(distTmp, .75))
-    myThresh <- (c75-c25)+c75
-    out <- distTmp>myThresh
-  }
+  pedTMBScores$Type <- "Pediatric"
+  adultTMBScores$Type <- "Adult"
+  tmbScores <- rbind(pedTMBScores, adultTMBScores)
   
+  # count median per histology
+  disease.order <- tmbScores %>%
+    group_by(Type, Diseasetype) %>%
+    summarise(median = median(TMBscore), count = n()) %>%
+    filter(count > 2) %>%
+    arrange(desc(median)) %>%
+    .$Diseasetype
   
-  getMMRFreq <- function(tmpAnnot) {
-    #Define the Hypermutation profile
-    tmpAnnot[,"Outlier"] <- isOutlier(tmpAnnot[,"MUTATION_COUNT"])
-    tmpAnnot[,"EST_MUT_PER_MB"] <- tmpAnnot[,"MUTATION_COUNT"]/30
-    return(tmpAnnot)
-  }
-  number_ticks <- function(n) {function(limits) pretty(limits, n)}
+  # subset to disease type with > 2 samples
+  tmbScores <- tmbScores %>%
+    filter(Diseasetype %in% disease.order)
+  tmbScores$Diseasetype <- factor(tmbScores$Diseasetype, levels = disease.order)
   
-  
-  ######################
-  ##Read data 
-  ####################
-  
-  #Read in data 26504 to start
-  
-  ######################
-  ##Filter data 
-  ####################
-  
-  #Filter to studies that have mutation counts 15373
-  myTMB[,"MUTATION_COUNT"] <- as.numeric(myTMB[,"MUTATION_COUNT"])
-  myTMB <- myTMB[!is.na(myTMB[,"MUTATION_COUNT"]),]
-  myTMB[,"NAME"] <- as.character(myTMB[,"NAME"])
-  
-  #Remove PPTP and FMI
-  myTMB <- filter(myTMB, DISEASE.NAME!="Pediatric Preclinical Testing Program" & DISEASE.NAME!="Mixed Cancer Types")
-  
-  #Add Whether Pediatric or Adult
-  myTMB[,"Type"] <- "Adult"
-  pediatricCancers <- c("Neuroblastoma", "Medulloblastoma", "Acute Myeloid Leukemia", "Pediatric Ganglio", "Diffuse Intrinsic Pontine Glioma")
-  myTMB[myTMB[,"DISEASE.NAME"]%in%pediatricCancers, "Type"] <- "Pediatric"
-  
-  #Sort and order it in decreasing fashion 
-  grouped <- group_by(myTMB, Type, DISEASE.NAME)
-  tmpOut <- summarise(grouped, median=median(MUTATION_COUNT))
-  tmpOut <- arrange(tmpOut, Type, desc(median))
-  myOrder <- as.character(as.data.frame(tmpOut)[,"DISEASE.NAME"])
-  myTMB[,"DISEASE.NAME"] <- factor(myTMB[,"DISEASE.NAME"], levels=myOrder)
-  myTMB[,"EST_MUT_PER_MB"] <- myTMB[,"MUTATION_COUNT"]/30
-  
-  #Filter mutations to get TMB of samples
-  filtMut <- mutData 
-  filtMut <- filtMut[filtMut[,"HGVSp_Short"]!="",]
-  tmbSample <- nrow(filtMut)/30
-  
-  #Plot it
-  p <- ggplot(myTMB, aes(DISEASE.NAME, EST_MUT_PER_MB, fill=Type))+geom_boxplot()+theme_bw()
-  p <- p+scale_y_log10(breaks=c(.25, 1, 10, 100, 500))+scale_fill_manual(values=c("blue", "red"))
-  p <- p+xlab("Disease")+ylab("Est Mutation Count per MB (Log Scale)")+theme(axis.text.x = element_text(angle = -90, hjust = (0)))
-  p <- p+ggtitle("Mutation Count versus Disease")+geom_hline(yintercept=tmbSample, linetype=2)
+  # Plot it
+  p <- ggplot(tmbScores, aes(Diseasetype, TMBscore, fill = Type)) + 
+    geom_boxplot() + theme_bw() + scale_y_log10(breaks = c(.25, 1, 10, 100, 500)) + 
+    scale_fill_manual(values = c("blue", "red")) + 
+    xlab("Disease") + ylab("Mutations per MB") + 
+    theme(axis.text.x = element_text(angle = -90, hjust = (0))) +
+    geom_hline(yintercept = TMB, linetype = 2)
   return(p)
-  
 }
