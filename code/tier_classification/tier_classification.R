6f1e16a9b18d39f7d9d6e199c69a234b021df2ce
runjin326 <47674661+runjin326@users.noreply.github.com>
2021-11-16 19:47:48 -0500

    cancer hotspot specific added

--- code/tier_classification/tier_classification.R
+++ code/tier_classification/tier_classification.R
@@ -31,6 +31,16 @@ if(nrow(oncokb_anno) == 0){
   hotspot_snv <- readr::read_tsv(file.path(data_dir, 'hotspots_database', 'hotspot_database_2017_snv.tsv')) %>%
     distinct()
   
+  # read in COSMIC resistance marker df
+  cosmic_resistance <- readr::read_tsv(file.path(data_dir, 'CosmicResistanceMutations.tsv')) %>% 
+    dplyr::select(`Gene Name`, `AA Mutation`, `CDS Mutation`) %>% 
+    dplyr::mutate(cosmic_resistance_variant = "Yes", 
+                  `Gene Name` = gsub("\\_.*", "", `Gene Name`)) %>% 
+    dplyr::rename(HGVSp_Short = `AA Mutation`, 
+                  HGVSc = `CDS Mutation`, 
+                  Hugo_Symbol = `Gene Name`) %>%
+    distinct()
+  
   # subset to genes in all_findings_output corresponding to VUS and Mutation
   genes_of_interest <- all_findings_output %>%
     filter(Type %in% c("VUS", "Mutation")) %>% 
@@ -38,7 +48,7 @@ if(nrow(oncokb_anno) == 0){
   hotspot_snv <- hotspot_snv %>% 
     filter(Hugo_Symbol %in% genes_of_interest)
   hotspot_indel <- hotspot_indel %>%
-    filter(Hugo_Symbol %in% hotspot_snv)
+    filter(Hugo_Symbol %in% hotspot_snv$Hugo_Symbol)
   
   # find the oncogenes tumor suppressor genes in OMPARE knowledge base
   oncogenes_tsg <- cancer_genes %>%
@@ -47,7 +57,8 @@ if(nrow(oncokb_anno) == 0){
   
   # calculate VAF for subsequent classification 
   oncokb_anno <- oncokb_anno %>% 
-    dplyr::mutate(VAF = t_alt_count/(t_alt_count+t_ref_count))
+    dplyr::mutate(VAF = t_alt_count/(t_alt_count+t_ref_count)) %>% 
+    left_join(cosmic_resistance)
   
   # Filter for Tier I
   oncokb_anno_tier1 <- oncokb_anno %>% 
@@ -62,18 +73,12 @@ if(nrow(oncokb_anno) == 0){
     dplyr::filter(gnomAD_AF<0.01| is.na(gnomAD_AF)) %>%
     # filter on VAF over 5% to avoid background noise
     dplyr::filter(VAF>0.05) %>%
-    # filter to contain only mutations that has COSMIC ID
-    dplyr::mutate(has_COSMIC = dplyr::case_when(
-      stringr::str_detect(Existing_variation,
-                          "COSM") ~ "Yes",
-      TRUE ~ "No"
-    )) %>%
-    dplyr::filter(has_COSMIC == "Yes") %>%
+    # filter to contain only mutations that has COSMIC ID or has resistance mutation
+    dplyr::filter(grepl("COSM", Existing_variation) | cosmic_resistance_variant == "Yes") %>% 
     # filter to contain only mutations with citations
     dplyr::filter(!is.na(CITATIONS)) %>% 
     # filter to TSG lists in OMPARE database
     dplyr::filter(Hugo_Symbol %in% oncogenes_tsg ) %>% 
-    dplyr::select(-has_COSMIC) %>% 
     dplyr::mutate(tier_classification = "TIER: I")
   
   
@@ -90,18 +95,12 @@ if(nrow(oncokb_anno) == 0){
     dplyr::filter(gnomAD_AF<0.01| is.na(gnomAD_AF)) %>%
     # filter on VAF over 5% to avoid background noise
     dplyr::filter(VAF>0.05) %>%
-    # filter to contain only mutations that has COSMIC ID
-    dplyr::mutate(has_COSMIC = dplyr::case_when(
-      stringr::str_detect(Existing_variation,
-                          "COSM") ~ "Yes",
-      TRUE ~ "No"
-    )) %>%
-    dplyr::filter(has_COSMIC == "Yes") %>%
+    # filter to contain only mutations that has COSMIC ID or has resistance mutation
+    dplyr::filter(grepl("COSM", Existing_variation) | cosmic_resistance_variant == "Yes") %>% 
     # filter to contain only mutations with citations
     dplyr::filter(!is.na(CITATIONS)) %>% 
     # filter to TSG lists in OMPARE database
     dplyr::filter(Hugo_Symbol %in% oncogenes_tsg ) %>% 
-    dplyr::select(-has_COSMIC) %>% 
     dplyr::mutate(tier_classification = "TIER: II")
   
   
@@ -145,29 +144,69 @@ if(nrow(oncokb_anno) == 0){
   ######################### annotate MSK SNV hotspot database to key clinical findings & all findings table
   
   # annotate SNV first
-  if(nrow(hotspot_snv)){
+  if(nrow(hotspot_snv)>0){
+    # modify the clinical output files for easier manipulation
+    all_findings_output_snv <- all_findings_output %>% 
+      dplyr::filter(grepl("Missense_Mutation", Details) | grepl("Nonsense_Mutation", Details) | grepl("Splice_Site", Details) | grepl("Splice_Region", Details)) %>%
+      dplyr::mutate(position = gsub(".*HGVSp: p..", "", Details)) %>% 
+      dplyr::mutate(position =str_sub(position, end = -2))
+    
+    key_findings_output_snv <- key_findings_output %>% 
+      dplyr::filter(grepl("Missense_Mutation", Details) | grepl("Nonsense_Mutation", Details) | grepl("Splice_Site", Details) | grepl("Splice_Region", Details)) %>%
+      dplyr::mutate(position = gsub(".*HGVSp: p..", "", Details)) %>% 
+      dplyr::mutate(position =str_sub(position, end = -2))
+      
     all_findings_output_hotspots <- data.frame()
     key_clinical_findings_output_hotspots <- data.frame()
     
-    for(p in 1:nrow(hotspot_snv)){
+    snv_location <- hotspot_snv %>% 
+      dplyr::select(Hugo_Symbol, Amino_Acid_Position) %>% 
+      distinct()
+    
+    for(p in 1:nrow(snv_location)){
       
-      each_entry <- hotspot_snv[p,]
+      each_entry <- snv_location[p,]
       hotspot_gene <- each_entry$Hugo_Symbol
       hotspot_aa_position <- each_entry$Amino_Acid_Position
       
-      all_findings_output_hotspot <- all_findings_output %>%
-        dplyr::filter(Aberration == hotspot_gene & grepl(hotspot_aa_position, Details)) %>%
-        dplyr::filter(grepl("Missense_Mutation", Details) | grepl("Nonsense_Mutation", Details) | grepl("Splice_Site", Details) | grepl("Splice_Region", Details))%>%
-        dplyr::mutate(Variant_Properties= paste0("Cancer Hotspot Location; ", Variant_Properties))
+      # find all the specific AA mutations in the hotspot table
+      hotspot_aa <- hotspot_snv %>% 
+        dplyr::filter(Amino_Acid_Position == hotspot_aa_position,
+                      Hugo_Symbol==hotspot_gene) %>%
+        dplyr::pull(HGVSp_short)
       
+      # reformat to make it matchable 
+      hotspot_aa<-paste(noquote(hotspot_aa),collapse='|')
+      
+      # first deal with all clinical findings table
+      all_findings_output_hotspot <- all_findings_output_snv %>%
+        dplyr::filter(Aberration == hotspot_gene,
+                      position == hotspot_aa_position) %>%
+        dplyr::select(-position)
+      
+      if(nrow(all_findings_output_hotspot)>0){
+        all_findings_output_hotspot <- all_findings_output_hotspot %>%
+          dplyr::mutate(Variant_Properties= case_when(
+            grepl(hotspot_aa, Details) ~ paste0("Cancer Hotspot; ", Variant_Properties),
+            TRUE ~ paste0("Cancer Hotspot Location; ", Variant_Properties)
+          ))
+        }
       all_findings_output_hotspots <- bind_rows(all_findings_output_hotspots, all_findings_output_hotspot)
       
-      key_clinical_findings_output_hotspot <- key_clinical_findings_output %>% 
-        dplyr::filter(Aberration == hotspot_gene & grepl(hotspot_aa_position, Details)) %>%
-        dplyr::filter(grepl("Missense_Mutation", Details) | grepl("Nonsense_Mutation", Details) | grepl("Splice_Site", Details) | grepl("Splice_Region", Details))%>%
-        dplyr::mutate(Variant_Properties= paste0("Cancer Hotspot Location; ", Variant_Properties))
-      
-      key_clinical_findings_output_hotspots <- bind_rows(key_clinical_findings_output_hotspots, key_clinical_findings_output_hotspot)
+      # now deal with key clinical findings
+      key_findings_output_hotspot <- key_findings_output_snv %>%
+        dplyr::filter(Aberration == hotspot_gene,
+                      position == hotspot_aa_position) %>%
+        dplyr::select(-position)
+
+      if(nrow(key_findings_output_hotspot)>0){
+        key_findings_output_hotspot <- key_findings_output_hotspot %>%
+          dplyr::mutate(Variant_Properties= case_when(
+            grepl(hotspot_aa, Details) ~ paste0("Cancer Hotspot; ", Variant_Properties),
+            TRUE ~ paste0("Cancer Hotspot Location; ", Variant_Properties)
+          ))
+      }
+      key_findings_output_hotspots <- bind_rows(key_findings_output_hotspots, key_findings_output_hotspot)
     }
     
     # add the annotations to the file 
@@ -184,27 +223,32 @@ if(nrow(oncokb_anno) == 0){
   ######################### annotate MSK Indel hotspot database to key clinical findings & all findings table
   
   # annotate Indel now
-  if(nrow(hotspot_indel)){
+  if(nrow(hotspot_indel)>0){
     all_findings_output_indels <- data.frame()
     key_clinical_findings_output_indels <- data.frame()
     
+    all_findings_output_mnv <- all_findings_output %>%
+      dplyr::filter(grepl("Frame_Shift_Del", Details) | grepl("Frame_Shift_Ins", Details) | grepl("In_Frame_Del", Details) | grepl("In_Frame_Ins", Details))
+      
+    key_findings_output_mnv <- key_findings_output %>%
+      dplyr::filter(grepl("Frame_Shift_Del", Details) | grepl("Frame_Shift_Ins", Details) | grepl("In_Frame_Del", Details) | grepl("In_Frame_Ins", Details))
+      
     for(q in 1:nrow(hotspot_indel)){
       
       each_indel <- hotspot_indel[q,]
       hotspot_gene_indel <- each_indel$Hugo_Symbol
-      hotspot_aa_start <- each_indel$Amino_Acid_Start
-      hotspot_aa_end <- each_indel$Amino_Acid_End
+      hotspot_specific_indel <- each_indel$HGVSp_short
       
-      all_findings_output_indel <- all_findings_output %>%
-        dplyr::filter(Aberration == hotspot_gene_indel & grepl(hotspot_aa_start, Details) & grepl(hotspot_aa_end, Details)) %>%
-        dplyr::filter(grepl("Frame_Shift_Del", Details) | grepl("Frame_Shift_Ins", Details) | grepl("In_Frame_Del", Details) | grepl("In_Frame_Ins", Details))%>%
-        mutate(Variant_Properties= paste0("Cancer Hotspot; ", Variant_Properties))
+      all_findings_output_indel <- all_findings_output_mnv %>%
+        dplyr::filter(Aberration == hotspot_gene_indel) %>%
+        dplyr::filter(grepl(hotspot_specific_indel, Details)) %>%
+        dplyr::mutate(Variant_Properties= paste0("Cancer Hotspot; ", Variant_Properties))
       all_findings_output_indels <- bind_rows(all_findings_output_indel, all_findings_output_indels)
       
-      key_clinical_findings_output_indel <- key_clinical_findings_output %>%
-        dplyr::filter(Aberration == hotspot_gene_indel & grepl(hotspot_aa_start, Details) & grepl(hotspot_aa_end, Details)) %>%
-        dplyr::filter(grepl("Frame_Shift_Del", Details) | grepl("Frame_Shift_Ins", Details) | grepl("In_Frame_Del", Details) | grepl("In_Frame_Ins", Details))%>%
-        mutate(Variant_Properties= paste0("Cancer Hotspot; ", Variant_Properties))
+      key_clinical_findings_output_indel <- key_findings_output_mnv %>%
+        dplyr::filter(Aberration == hotspot_gene_indel) %>%
+        dplyr::filter(grepl(hotspot_specific_indel, Details)) %>%
+        dplyr::mutate(Variant_Properties= paste0("Cancer Hotspot; ", Variant_Properties))
       key_clinical_findings_output_indels <- bind_rows(key_clinical_findings_output_indel, key_clinical_findings_output_indels)
     }
     
