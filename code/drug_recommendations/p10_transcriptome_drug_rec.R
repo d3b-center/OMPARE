26c0c88c93f2d3f937566cc16fb3f59c87a949ce
runjin326 <47674661+runjin326@users.noreply.github.com>
2021-11-23 15:22:51 -0500

    add hub_gsnca to dge output

--- code/drug_recommendations/p10_transcriptome_drug_rec.R
+++ code/drug_recommendations/p10_transcriptome_drug_rec.R
@@ -16,9 +16,25 @@ full_fname <- file.path(output_dir, paste0(patient, "_CHEMBL_drug-gene.tsv"))
 transcriptome_drug_rec_output <- transcriptome_drug_rec(diffexpr_genes = genes_up)
 write.table(x = transcriptome_drug_rec_output, file = full_fname, quote = F, sep = "\t", row.names = F)
 
+# annotate GSNCA hub gene information to annotation
+gsnca_results_dir <- file.path(patient_dir, "output", "gsnca_analysis")
+gsnca_result_gtex <- readr::read_tsv(file.path(gsnca_results_dir, "top20_similar_vs_GTEx_GSNCA_analysis.tsv"))
+gsnca_result_pbta_all <- readr::read_tsv(file.path(gsnca_results_dir, "top20_similar_vs_PBTA_all_GSNCA_analysis.tsv"))
+gsnca_result_pbta_hgg <- readr::read_tsv(file.path(gsnca_results_dir, "top20_similar_vs_PBTA_HGG_GSNCA_analysis.tsv"))
+
+# annotate GSNCA hub gene
+transcriptome_drug_rec_output <- transcriptome_drug_rec_output %>% 
+  mutate(hub_GSNCA = case_when(
+    grepl("GTEx", Comparison) & Gene %in% gsnca_result_gtex$hub_gene ~ "Yes",
+    grepl("PBTA_ALL", Comparison) & Gene %in% gsnca_result_pbta_all$hub_gene ~ "Yes",
+    grepl("PBTA_HGG", Comparison) & Gene %in% gsnca_result_pbta_hgg$hub_gene ~ "Yes",
+    TRUE ~ "No"
+  )
+)
+
 # subset columns for report
 fname <- file.path(output_dir, "transcriptome_drug_rec.rds")
 transcriptome_drug_rec_output <- transcriptome_drug_rec_output %>%
-  dplyr::select(Drug, Gene, Source, Comparison, logFC, MOA) %>%
+  dplyr::select(Drug, Gene, Source, Comparison, logFC, MOA, hub_GSNCA) %>%
   unique()
 saveRDS(transcriptome_drug_rec_output, file = fname)
