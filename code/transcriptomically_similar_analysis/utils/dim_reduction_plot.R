45b1a1ad25b06608557d7296acf430a22e250c01
komalsrathi <komal.rathi@gmail.com>
2022-02-03 14:05:53 -0500

    format size and shape

--- code/transcriptomically_similar_analysis/utils/dim_reduction_plot.R
+++ code/transcriptomically_similar_analysis/utils/dim_reduction_plot.R
@@ -14,32 +14,25 @@ dim_reduction_plot <- function(dat, clindata, study, patient, title) {
                  rownames_to_column('tmp'), by = 'tmp') %>%
     column_to_rownames('tmp')
   
-  # reverse factors to set PNOC to lowest
-  # this is for shape
-  dat$label <- dat$study_id
-  dat$label <- ifelse(dat$subject_id == patient, patient, dat$label)
-  dat$label <- factor(dat$label, levels = c(study, "PNOC008", patient))
-  
-  # this is for size
-  dat$study_id <- as.factor(dat$study_id)
-  dat$study_id <- relevel(dat$study_id, ref = "PNOC008")
-  dat$study_id <- fct_rev(dat$study_id)
+  # shape
+  dat$label <- ifelse(dat$subject_id == patient, 'patient_of_interest', dat$study_id)
+  dat$label <- as.factor(dat$label)
+  dat$label <- relevel(dat$label, ref = 'patient_of_interest')
   
   # plot
   p <- ggplot(dat, aes(Dim1, Dim2,
                        color = short_histology,
-                       size = study_id,
                        shape = label,
                        text = paste0("Sample:", subject_id,
                                      "\nShort_histology:", short_histology,
                                      "\nBroad_histology:", broad_histology,
                                      "\nPathology_diagnosis:", pathology_diagnosis,
                                      "\nIntegrated_diagnosis:", integrated_diagnosis))) +
-    geom_jitter(width = 0.5, height = 0.5) +
+    geom_jitter(width = 0.5, height = 0.5, size = 2, alpha = 0.8) +
     theme_bw() + ggtitle(title) +
     theme(plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
-          legend.title=element_text(size = 8),
-          legend.text=element_text(size = 8)) +
-    guides(size = FALSE, shape = F, color = F)
+          legend.title = element_text(size = 8),
+          legend.text = element_text(size = 8)) +
+    guides(shape = "none", color = "none", size = "none", alpha = "none")
   return(p)
 }
